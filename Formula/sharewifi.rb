7b1372511aad4ab271a48c903c92b8f44b70e8fa
Brady Joslin <brady@bradyjoslin.com>
2020-07-31 19:21:12 -0500

    Update sharewifi.rb

--- Formula/sharewifi.rb
+++ Formula/sharewifi.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class WifiPassword < Formula
+class Sharewifi < Formula
     desc "Quickly share Wi-Fi connection info"
     homepage "https://github.com/bradyjoslin/sharewifi"
     url "https://github.com/bradyjoslin/sharewifi/releases/download/v0.1.4/sharewifi-mac.tar.gz"
