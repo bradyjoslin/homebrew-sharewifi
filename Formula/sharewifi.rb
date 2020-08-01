8226444f29462dde70a8f100c0413d85183e37b0
Brady Joslin <brady@bradyjoslin.com>
2020-08-01 14:47:29 -0500

    Update sharewifi.rb

--- Formula/sharewifi.rb
+++ Formula/sharewifi.rb
@@ -6,7 +6,7 @@ class Sharewifi < Formula
     homepage "https://github.com/bradyjoslin/sharewifi"
     url "https://github.com/bradyjoslin/sharewifi/releases/download/v0.1.5/sharewifi-mac.tar.gz"
     sha256 "d732bf53035583f7cc84fae95db722fd027704c5e31926d5b0cb13abf2485e56"
-    version "0.1.4"
+    version "0.1.5"
   
     def install
       bin.install "sharewifi"
