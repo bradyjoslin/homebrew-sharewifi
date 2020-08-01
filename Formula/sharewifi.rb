119684e589d5a08a295ebe4bc6c0a6301192bad8
Brady Joslin <brady@bradyjoslin.com>
2020-07-31 19:19:47 -0500

    Update sharewifi.rb

--- Formula/sharewifi.rb
+++ Formula/sharewifi.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class WifiPassword < Formula
     desc "Quickly share Wi-Fi connection info"
-    homepage "https://github.com/bradyjoslin/wifi-password"
-    url "https://github.com/bradyjoslin/wifi-password/releases/download/v0.1.4/sharewifi-mac.tar.gz"
+    homepage "https://github.com/bradyjoslin/sharewifi"
+    url "https://github.com/bradyjoslin/sharewifi/releases/download/v0.1.4/sharewifi-mac.tar.gz"
     sha256 "c8449dc7cb34a1ef06cbd9ef10a99dc7c6f91bdc8b45e88c80231bf17ca4ec18"
     version "0.1.4"
   
